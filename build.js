b450881b138d0dc5be28756f85a5817336086db8
Eric Jinks <ericjinks@gmail.com>
2019-08-16 10:36:08 +1000

    Update html template

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const buildTime = new Date()
 
 const html = `<!DOCTYPE html>
 <html lang="en">
@@ -8,9 +9,27 @@ const html = `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Github Actions</title>
+    <style>
+      body {
+        font-family: system-ui, sans-serif;
+        display: flex;
+        height: 100vh;
+        margin: 0;
+        text-align: center;
+      }
+      div {
+        margin: auto;
+      }
+      a {
+        color: slateblue;
+      }
+    </style>
   </head>
   <body>
-    <h1 id="timestamp">Last Build: ${new Date().toString()}</h1>
+    <div>
+      <h2><a href="https://github.com/Jinksi/netlify-build-github-actions">Netlify build triggered by Github Actions</a></h1>
+      <h1><time datetime="${buildTime.toISOString()}">${buildTime.toUTCString()}</time></h1>
+    </div>
   </body>
 </html>
 `
